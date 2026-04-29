bfd3210e4dd22ef6a514311ed374f870c9bb15a8
D4nzyXcripter <ampremcuki123@gmail.com>
2026-04-29 16:14:37 +0700

    Update mainsourc2424][[o]qflweme2324eiuhiuhiwhu2.lua

--- mainsourc2424][[o]qflweme2324eiuhiuhiwhu2.lua
+++ mainsourc2424][[o]qflweme2324eiuhiuhiwhu2.lua
@@ -428,7 +428,7 @@ elseif authData.type == "limit" then
 end
 
 local success1, err1 = pcall(function()
-    loadstring(game:HttpGet("https://pastefy.app/HHJetwLQ/raw"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/D4nzyXcripter/Get-Script/refs/heads/main/gg"))()
 end)
 if not success1 then
     warn("⚠️ Script 1 gagal: " .. tostring(err1))
